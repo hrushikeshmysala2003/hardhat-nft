bb984ec56dec1c7c019f73e697f0d0b82b51b18f
rushi <hrushikeshmysala2003@gmail.com>
2024-02-26 18:41:47 +0530

    Compiled a stater version od basicnft

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomiclabs/hardhat-waffle")
 require("hardhat-gas-reporter")
-require("@nomiclabs/hardhat-verify")
+require("@nomicfoundation/hardhat-verify")
 require("dotenv").config()
 require("solidity-coverage")
 require("hardhat-deploy")
@@ -16,6 +16,7 @@ const SEPOLIA_RPC_URL =
     "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY"
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
 
 module.exports = {
     defaultNetwork: "hardhat",
